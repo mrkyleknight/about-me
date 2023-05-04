ea6263233c8b077bce53ec0ec89b216d018f6d2c
RocioMartinez <rym5411@gmail.com>
2023-05-04 17:26:06 -0400

    function 6

--- js/app.js
+++ js/app.js
@@ -15,13 +15,13 @@ alert(`Welcome to my site ${userName}! Lets play some trivia about Kyle Knight!
 
 let numCorrect = 0;
 
-function kyleBirds(){
+function kyleBirds() {
 
   let questionOneGuess = prompt('Does Kyle own birds? True (T) or False (F)?').toLowerCase();
-  if(questionOneGuess === 't' || questionOneGuess === 'true'){
+  if (questionOneGuess === 't' || questionOneGuess === 'true') {
     alert('True!. Kyle has three birds, a Cockatiel, Lovebird and a Green Conure');
     numCorrect++;
-  } else if(questionOneGuess === 'f' || questionOneGuess === 'false'){
+  } else if (questionOneGuess === 'f' || questionOneGuess === 'false') {
     alert(`False! Kyle has three birds, a Cockatiel, Lovebird and a Green Conure`);
   }
 
@@ -30,15 +30,15 @@ function kyleBirds(){
 kyleBirds();
 
 
-function kyleDriving(){
+function kyleDriving() {
 
   let questionTwoGuess = prompt(`Kyle has driven over a Million miles? True (T) or False? (F)`).toLowerCase();
-  
-  if(questionTwoGuess === 't' || questionTwoGuess === 'true'){
+
+  if (questionTwoGuess === 't' || questionTwoGuess === 'true') {
     alert(`True! Kyle drove over a million miles spanning over a decade as a Truck Driver. Sommetimes averaging 500 miles a day `);
     numCorrect++;
-  
-  } else if (questionTwoGuess === 'f' || questionTwoGuess === 'false'){
+
+  } else if (questionTwoGuess === 'f' || questionTwoGuess === 'false') {
     alert(`Wrong! Kyle drove over a million miles spanning over a decade as a Truck Driver. Sometimes averaging 500 miles a day`);
   }
 
@@ -46,15 +46,15 @@ function kyleDriving(){
 
 kyleDriving();
 
-function kyleRuns(){
+function kyleRuns() {
 
   let questionThreeGuess = prompt(`Kyle has completed two marathons? True (T) or False? (F)`).toLowerCase();
-  
-  if(questionThreeGuess === 't' || questionThreeGuess === 'true'){
+
+  if (questionThreeGuess === 't' || questionThreeGuess === 'true') {
     alert(`False! Kyle hates running`);
     numCorrect++;
-  
-  } else if (questionThreeGuess === 'f' || questionThreeGuess === 'false'){
+
+  } else if (questionThreeGuess === 'f' || questionThreeGuess === 'false') {
     alert(`True! Kyle hates running`);
   }
 
@@ -62,15 +62,15 @@ function kyleRuns(){
 
 kyleRuns();
 
-function kyleHome(){
+function kyleHome() {
 
   let questionFourGuess = prompt(`Kyle lives in Washington DC? True (T) or False? (F)`).toLowerCase();
-  
-  if(questionFourGuess === 't' || questionFourGuess === 'true'){
+
+  if (questionFourGuess === 't' || questionFourGuess === 'true') {
     alert(`False! Kyle lives in Washington State`);
-     numCorrect++;
-  
-  } else if (questionFourGuess === 'f' || questionFourGuess === 'false'){
+    numCorrect++;
+
+  } else if (questionFourGuess === 'f' || questionFourGuess === 'false') {
     alert(`True! Kyle lives in Washington State`);
   }
 
@@ -79,15 +79,15 @@ function kyleHome(){
 kyleHome();
 
 
-function kyleGames(){
+function kyleGames() {
 
   let questionFiveGuess = prompt(`Kyle prefers Xbox over Playstation? True (T) or False? (F)`).toLowerCase();
-  
-  if(questionFiveGuess === 't' || questionFiveGuess === 'true'){
+
+  if (questionFiveGuess === 't' || questionFiveGuess === 'true') {
     alert(`True! Kyle has a 16 year old Xbox account,and games every weekend`);
-     numCorrect++;
-  
-  } else if (questionFiveGuess === 'f' || questionFiveGuess === 'false'){
+    numCorrect++;
+
+  } else if (questionFiveGuess === 'f' || questionFiveGuess === 'false') {
     alert(`False! Kyle hates Playstation, prefers Xbox`);
   }
 
@@ -98,27 +98,33 @@ kyleGames();
 const correctAnswer = 31;
 let attempts = 4;
 
-while (attempts > 0) {
- const guess = prompt("Guess Kyle's Age? between 1 and 100. You have " + attempts + " attempts remaining.");
+function kyleAge() {
 
- if (isNaN(guess)) {
- alert("Invalid input. Please enter a number between 1 and 100.");
- } else if (guess == correctAnswer) {
- alert("Congratulations! You guessed the correct age.");
-  numCorrect++;
- break;
- } else if (guess < correctAnswer) {
- alert("Too low. Try again.");
- } else if (guess > correctAnswer) {
- alert("Too high. Try again.");
- }
+  while (attempts > 0) {
+    const guess = prompt("Guess Kyle's Age? between 1 and 100. You have " + attempts + " attempts remaining.");
 
- attempts--;
+    if (isNaN(guess)) {
+      alert("Invalid input. Please enter a number between 1 and 100.");
+    } else if (guess == correctAnswer) {
+      alert("Congratulations! You guessed the correct age.");
+      numCorrect++;
+      break;
+    } else if (guess < correctAnswer) {
+      alert("Too low. Try again.");
+    } else if (guess > correctAnswer) {
+      alert("Too high. Try again.");
+    }
+
+    attempts--;
+  }
+
+  if (attempts == 0) {
+    alert("Sorry, you ran out of attempts. The correct age was " + correctAnswer + ".");
+  }
+
 }
 
-if (attempts == 0) {
- alert("Sorry, you ran out of attempts. The correct age was " + correctAnswer + ".");
-}
+kyleAge();
 
 const correct2ndAnswers = [31, 32, 33];
 let attempts2 = 6;
@@ -130,13 +136,13 @@ while (attempts2 > 0) {
     alert("Invalid input. Please enter a number between 1 and 100.");
   } else if (correct2ndAnswers.includes(Number(guess))) {
     alert("Congratulations! You guessed one of my lucky numbers. 31, 32, 33");
-     numCorrect++;
+    numCorrect++;
     break;
   } else if (guess < correct2ndAnswers) {
     alert("Too low. Try again.");
   } else if (guess > correct2ndAnswers) {
     alert("Too high. Try again.");
-  
+
   }
 
   attempts2--;
@@ -146,4 +152,4 @@ if (attempts2 == 0) {
   alert("Sorry, you ran out of attempts. The correct numbers were: " + correct2ndAnswers.join(", ") + ".");
 }
 
-alert (`You got ${numCorrect} out of 7 correct!`);
+alert(`You got ${numCorrect} out of 7 correct!`);
