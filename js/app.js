bea674cd4faf20667ce28e4c8007b989cfa055a6
Kyle Knight <knightkyle@outlook.com>
2023-05-03 17:53:55 -0700

    Upload Lab 3

--- js/app.js
+++ js/app.js
@@ -4,26 +4,24 @@ console.log("Hey World");
 
 
 
-// Ask the user their name through a prompt()
 
 let userName = prompt('What is your name?');
 
-
-// Display that name back to the user through a custom greeting welcoming them to your site.
 
 
 alert(`Welcome to my site ${userName}! Lets play some trivia about Kyle Knight!
 
 “The worst thing about prison was the dementors.” – Michael “Prison Mike” Scott, Season 3`);
 
-// Display the user’s name back to them in your final message to the user.
-
-// Prompt the user a total of exactly five yes or no questions. The user input for the answer must accept either y/n or yes/no responses while taking into consideration case sensitivity by normalizing the user input so that it can be validated
+let numCorrect = 0;
 
 let questionOneGuess = prompt('Does Kyle own birds? True (T) or False (F)?').toLowerCase();
 
+
+
 if(questionOneGuess === 't' || questionOneGuess === 'true'){
   alert('True!. Kyle has three birds, a Cockatiel, Lovebird and a Green Conure');
+   numCorrect++;
 
 } else if(questionOneGuess === 'f' || questionOneGuess === 'false'){
   alert(`False! Kyle has three birds, a Cockatiel, Lovebird and a Green Conure`);
@@ -34,6 +32,7 @@ let questionTwoGuess = prompt(`Kyle has driven over a Million miles? True (T) or
 
 if(questionTwoGuess === 't' || questionTwoGuess === 'true'){
   alert(`True! Kyle drove over a million miles spanning over a decade as a Truck Driver. Sommetimes averaging 500 miles a day `);
+   numCorrect++;
 
 } else if (questionTwoGuess === 'f' || questionTwoGuess === 'false'){
   alert(`False! Kyle drove over a million miles spanning over a decade as a Truck Driver. Sommetimes averaging 500 miles a day`);
@@ -43,6 +42,7 @@ let questionThreeGuess = prompt(`Kyle has completed two marathons? True (T) or F
 
 if(questionThreeGuess === 't' || questionThreeGuess === 'true'){
   alert(`False! Kyle hates running`);
+  numCorrect++;
 
 } else if (questionThreeGuess === 'f' || questionThreeGuess === 'false'){
   alert(`True! Kyle hates running`);
@@ -52,6 +52,7 @@ let questionFourGuess = prompt(`Kyle lives in Washington DC? True (T) or False? 
 
 if(questionFourGuess === 't' || questionFourGuess === 'true'){
   alert(`False! Kyle lives in Washingston State`);
+   numCorrect++;
 
 } else if (questionFourGuess === 'f' || questionFourGuess === 'false'){
   alert(`True! Kyle lives in Washington State`);
@@ -61,7 +62,62 @@ let questionFiveGuess = prompt(`Kyle prefers Xbox over Playstation? True (T) or 
 
 if(questionFiveGuess === 't' || questionFiveGuess === 'true'){
   alert(`True! Kyle has a 16 year old Xbox account,and games every weekend`);
+   numCorrect++;
 
 } else if (questionFiveGuess === 'f' || questionFiveGuess === 'false'){
   alert(`False! Kyle hates Playstation, prefers Xbox`);
 }
+
+const correctAnswer = 31;
+let attempts = 4;
+
+while (attempts > 0) {
+ const guess = prompt("Guess Kyle's Age? between 1 and 100. You have " + attempts + " attempts remaining.");
+
+ if (isNaN(guess)) {
+ alert("Invalid input. Please enter a number between 1 and 100.");
+ } else if (guess == correctAnswer) {
+ alert("Congratulations! You guessed the correct age.");
+  numCorrect++;
+ break;
+ } else if (guess < correctAnswer) {
+ alert("Too low. Try again.");
+ } else if (guess > correctAnswer) {
+ alert("Too high. Try again.");
+ }
+
+ attempts--;
+}
+
+if (attempts == 0) {
+ alert("Sorry, you ran out of attempts. The correct age was " + correctAnswer + ".");
+}
+
+const correct2ndAnswers = [31, 32, 33];
+let attempts2 = 6;
+
+while (attempts2 > 0) {
+  const guess = prompt("Guess Kyle's Favorite Numbers? between 1 and 100. You have " + attempts2 + " attempts remaining.");
+
+  if (isNaN(guess)) {
+    alert("Invalid input. Please enter a number between 1 and 100.");
+  } else if (correct2ndAnswers.includes(Number(guess))) {
+    alert("Congratulations! You guessed the correct number.");
+     numCorrect++;
+    break;
+  } else if (guess < Math.min(...correct2ndAnswers)) {
+    alert("Too low. Try again.");
+  } else if (guess > Math.max(...correct2ndAnswers)) {
+    alert("Too high. Try again.");
+  } else {
+    alert("Sorry, your guess was not correct. Please try again.");
+  }
+
+  attempts2--;
+}
+
+if (attempts2 == 0) {
+  alert("Sorry, you ran out of attempts. The correct numbers were: " + correct2ndAnswers.join(", ") + ".");
+}
+
+alert (`You got ${numCorrect} out of 7 correct!`);
